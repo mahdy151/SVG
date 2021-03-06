ad95550c87261db8eaef22682001485ecd840390
mahdy151 <mahdy151saadat@gmail.com>
2021-03-06 21:08:41 +0330

    master

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-var tl = gsap.timeline({delay:0,ease:"circ.out"})
-    .to(".transition li", {duration: 0.3, scaleY:1,stagger:0.3})
+window.addEventListener('load', (event) => {
+  var tl = gsap.timeline({delay:0.3,ease:"expo.in"})
+    .set("path", {fill:"white",stroke:"white"})
     .from("path",{duration: 1.5, y:200, rotation:10, transformOrigin:"left bottom",stagger:0.1})
-    .to("path",{duration: 0.5, y:50, transformOrigin:"left bottom",stagger:{each:0.1,from:"end"}},"+=0.5")
-    .to("path",{duration: 0.5, y:-150, autoAlpha:0, transformOrigin:"left bottom",stagger:{each:0.1,from:"end"}},"-=1")
-    .to(".transition li", {duration: 0.35, scaleY:0,stagger:{each:0.25,from:"end"}})
-    .to(".preloader", {duration: 0.3, autoAlpha:0},"-=0.3");
+    .to("path",{duration: 0.7, y:-150, autoAlpha:0, transformOrigin:"left bottom"},"+=1")
+    .to("#bg1 , #bg2", {duration: 0.6, scaleX:0,stagger:0.1});
+});
+
