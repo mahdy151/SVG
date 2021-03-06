42c91ca9fb7930f80205b6ce43637372183bca3e
mahdy151 <mahdy151saadat@gmail.com>
2021-03-06 22:18:57 +0330

    master

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-window.addEventListener('load', (event) => {
-  var tl = gsap.timeline({delay:0.3,ease:"expo.in"})
+var img = document.createElement("img");
+img.src = "./IMG_۲۰۲۱۰۱۱۲_۲۳۲۵۱۸_1-Edit-3.jpg";
+img.onload = function() {
+    var tl = gsap.timeline({delay:0.3,ease:"expo.in"})
     .set("path", {fill:"white",stroke:"white"})
     .from("path",{duration: 1.5, y:200, rotation:10, transformOrigin:"left bottom",stagger:0.1})
     .to("path",{duration: 0.7, y:-150, autoAlpha:0, transformOrigin:"left bottom"},"+=1")
-    .to("#bg1 , #bg2", {duration: 0.6, scaleX:0,stagger:0.1});
-});
+    .to("#bg1 , #bg2", {duration: 0.6, scaleX:0,stagger:0.1})
+};
+// document.getElementsByTagName("image").load = function() {
+//   var tl = gsap.timeline({delay:0.3,ease:"expo.in"})
+//     .set("path", {fill:"white",stroke:"white"})
+//     .from("path",{duration: 1.5, y:200, rotation:10, transformOrigin:"left bottom",stagger:0.1})
+//     .to("path",{duration: 0.7, y:-150, autoAlpha:0, transformOrigin:"left bottom"},"+=1")
+//     .to("#bg1 , #bg2", {duration: 0.6, scaleX:0,stagger:0.1});
+// };
 
