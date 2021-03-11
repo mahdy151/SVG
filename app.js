10fe8163d30960b0710ef54a4069694cca4a46ea
mahdy151 <mahdy151saadat@gmail.com>
2021-03-11 15:30:39 +0330

    gsap fixed

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ img.onload = function() {
     var tl = gsap.timeline({delay:0.5,ease:"expo.in"})
     .set("#mask1 path", {stroke:"white"})
     .fromTo("#mask1 path", {drawSVG:"100% 100%"}, {duration: 3.0, drawSVG:"0% 100%",stagger:0.5})
-    .to(".preloader svg", {duration: 0.5, fill:"white"},"-=0.2")
+    .to(".preloader svg", {duration: 0.5, fill:"white"})
     .to("#mask1 path", {duration: 0.7, ease: "back.out(3)", scale:1.15, transformOrigin:"center center"}, "-=0.3")
     .to("#mask1 path", {duration: 0.8, autoAlpha:0},"+=1")
     .to("#bg1 , #bg2", {duration: 0.8, scaleX:0,stagger:0.1});
